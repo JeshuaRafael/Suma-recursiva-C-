f082712468fd37ab95bbe9fab1cd52632ed89c19
hp omen <hp omen@DESKTOP-I0EO1P5>
2023-10-29 23:36:59 -0600

    Agregar archivos de proyecto.

---
+++ Suma recursiva C#/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+class Program
+{
+    static int SumaRecursiva(int n)
+    {
+        if (n <= 9)
+            return n;
+        else
+            return SumaRecursiva(n / 10) + n % 10;
+    }
+
+    static void Main(string[] args)
+    {
+        Console.Write("Ingresa un número entero: ");
+        int numero = int.Parse(Console.ReadLine());
+
+        int resultado = SumaRecursiva(numero);
+        Console.WriteLine($"La suma de los dígitos de {numero} es: {resultado}");
+    }
+}
